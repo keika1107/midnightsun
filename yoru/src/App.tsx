b00723933d8a533935107eb6bbdd28110dd5233a
keika1107 <keika1107wolf@gmail.com>
2026-04-22 14:26:42 +0900

    Delete yoru directory

--- yoru/src/App.tsx
+++
@@ -1,4 +0,0 @@
-// Empty component - all content is inlined in index.html
-export default function App() {
-  return null;
-}
--- yoru/src/main.tsx
+++
@@ -1,3 +0,0 @@
-// Minimal entry point for Vite build compatibility
-// All application logic is inlined in index.html
-export default {};
